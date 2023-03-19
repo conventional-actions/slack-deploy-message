3be37b1712c77f6515e36b2b7251c1337f1a0fd7
Seth Yates <29878+sethyates@users.noreply.github.com>
2023-03-19 15:44:51 -0700

    chore: update

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -6,8 +6,7 @@ import {test} from '@jest/globals'
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
   process.env['INPUT_VERSION'] = 'v1.2.3'
-  process.env['INPUT_WEBHOOK-URL'] =
-    'https://hooks.slack.com/services/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'
+  process.env['INPUT_WEBHOOK-URL'] = ''
   process.env['GITHUB_REPOSITORY'] = 'ketch-com/bearing'
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
